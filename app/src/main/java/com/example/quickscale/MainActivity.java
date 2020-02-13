21670b50082ffe215a0e805a1bbf35bbec085a81
Valued Customer <Techandmusic34@gmail.com>
2020-02-12 18:34:29 -0600

    Spinners working. Now to finish the rest of MainActivity.

--- app/src/main/java/com/example/quickscale/MainActivity.java
+++ app/src/main/java/com/example/quickscale/MainActivity.java
@@ -1,26 +1,18 @@
 package com.example.quickscale;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
-
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
 
 
     public ScaleViewModel mViewModel;
@@ -32,30 +24,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public Chromatic mChromatic;
     public Scale mScale;
     public TextView scaleView;
+    public String[] notes = {"C", "C#/Db", "D"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateRootNoteSpinner();
+        populateScaleTypeSpinner();
         scaleView = findViewById(R.id.noteView);
         //Setup viewModel
-        mViewModel = new  ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(ScaleViewModel.class);
+        mViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(ScaleViewModel.class);
         //Initialize text in mViewModel
         mViewModel.setScaleNotes("Scale appears here");
         String scale = mViewModel.getScaleNotes();
         displayScale(scale);
 
-        //Create spinner for root note
-        rootNoteSpinner = (Spinner) findViewById(R.id.note_spinner);
-        //Create ArrayAdapter for rootNoteSpinner
-        ArrayAdapter<CharSequence> noteAdapter = ArrayAdapter.createFromResource(this, R.array.keys, android.R.layout.simple_spinner_item);
-        //Create Spinner for scale type
-        scaleTypeSpinner = (Spinner) findViewById(R.id.scale_spinner);
-        //Create ArrayAdapter for scaleTypeSpinner
-        ArrayAdapter<CharSequence>  scaleAdapter = ArrayAdapter.createFromResource(this, R.array.scales, android.R.layout.simple_spinner_item);
+
+
         //Specify layouts to use in Spinners
-        noteAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        scaleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
         //Setting OnItemSelectedListener for each Spinner
         rootNoteSpinner.setOnItemSelectedListener(this);
         scaleTypeSpinner.setOnItemSelectedListener(this);
@@ -72,23 +61,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         });
 
 
-
-
-
-
-
     }
+
+    public void populateRootNoteSpinner() {
+        //Create spinner for root note
+        rootNoteSpinner = (Spinner) findViewById(R.id.note_spinner);
+        //Create ArrayAdapter for rootNoteSpinner
+        ArrayAdapter<CharSequence> noteAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_dropdown_item, notes);
+        noteAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rootNoteSpinner.setAdapter(noteAdapter);
+    }
+     public void populateScaleTypeSpinner() {
+         //Create Spinner for scale type
+         scaleTypeSpinner = (Spinner) findViewById(R.id.scale_spinner);
+         //Create ArrayAdapter for scaleTypeSpinner
+         ArrayAdapter<CharSequence> scaleAdapter = ArrayAdapter.createFromResource(this, R.array.scales, android.R.layout.simple_spinner_item);
+         scaleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         scaleTypeSpinner.setAdapter(scaleAdapter);
+     }
+
+
 
     public void displayScale(String scales) {
         scaleView.setText(scales);
     }
-
-
-
-
-
-
-
 
 
     @Override
